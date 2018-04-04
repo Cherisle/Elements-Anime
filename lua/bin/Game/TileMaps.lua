fbd27cc88f54b2e9662803c1b82b818327e26b47
Nhel10 <lnyonzon@gmail.com>
2018-04-04 00:35:45 -0700

    Added TileMaps lua file
    Added TileMaps lua file that will act as a "Maps" function for all of
    the tile location in the Tile class. This will be used for adding and
    removing players from a specific active tile in the map

---
+++ lua/bin/Game/TileMaps.lua
@@ -0,0 +1,52 @@
+local Class = require("lib/middleclass")
+local Vector2 = require("lib/Vector2")
+
+local TileMaps = {}
+TileMaps._index = TileMaps
+
+
+function TileMaps:new()
+  local self = setmetatable({}, TileMaps)
+  self.arrayLoc = 0
+  self.key = {}
+  self.dataType = {}
+
+  return self
+end
+
+function TileMaps:add(self, k, d)
+  self.arrayLoc = self.arrayLoc + 1
+  self.key[self.arrayLoc] = k
+  self.dataType[self.arrayLoc] = d
+end
+
+
+
+function Tile:onTile(self, tileLoc, player)
+  loc = 0
+  for i = 1, table.getn(self.key), 1 do
+    if (tileLoc == self.key[i]) then
+      loc = i
+    end
+  end
+  nEmptyTile = table.getn(self.dataType[loc]) + 1
+  self.dataType[loc][nEmptyTile + 1] = player
+end
+
+function tile:offTile(self, tileLoc, player)
+  loc = 0
+  locPlayer = 0
+  for i = 1, table.getn(self.key), 1 do
+    if (tileLoc == self.key[i]) then
+      loc = i
+    end
+  end
+  for j = 1, table.getn(self.dataType[loc]), 1 do
+    if player.getLabel == self.dataType[loc][j].getLabel then
+      locPlayer = j
+    end
+  end
+  table.remove(self.dataType[loc], locPlayer)
+end
+
+return TileMaps 
