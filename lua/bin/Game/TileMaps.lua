28cf2bd2f02292e840caa995993eef4e172a1ecb
Neru <lnyonzon@gmail.com>
2018-04-23 16:52:19 -0700

    Theoretically finished TileMaps
    Added tile functionality to add and remove players from a tile using a TileMaps.lua that in a way acts as a C++ maps reference. TileMaps.lua has an array of keys that stores all of a specific tile location in a map, and an array of data type that will contain another array to store players that land on that tile location. Both key array and datatype array are parallel to each other so information stays consistent.
    
    This tile function does have a way to be tested yet until further PlayerGUI code is created .

--- lua/bin/Game/TileMaps.lua
+++ lua/bin/Game/TileMaps.lua
@@ -1,3 +1,15 @@
+--[[ Purpose: The purpose of this class is to imitate a Maps function
+     like the one in C++. This class is needed to keep track of all locations
+     of one specific tile within a map and have functions that can be used to add
+     players on to a tile and functions that can remove player from a tile on to
+     another tile. \
+
+     Just like the C++ Maps reference, TileMaps will contain parallel arrays one being
+     used for the key and the other as the dataType. The arrayLoc will be used to keep track
+     of how many arrays there are in the TileMaps class.
+
+]]
+
 local Class = require("lib/middleclass")
 local Vector2 = require("lib/Vector2")
 
@@ -8,21 +20,37 @@ TileMaps._index = TileMaps
 function TileMaps:new()
   local self = setmetatable({}, TileMaps)
   self.arrayLoc = 0
+
+  -- contains the location of the tile
   self.key = {}
+
+  -- contains the list of players on the location of the tile
+  -- in other words the dataType will be an array of all the players in the tile
   self.dataType = {}
 
   return self
 end
 
+function TileMaps:addTileLoc(self, tileLoc)
+  self.arrayLoc = self.arrayLoc + 1
+  self.key[self.arrayLoc] = tileLoc
+  self.dataType[self.arrayLoc] = {n=6}
+end
+
+
+--[[
 function TileMaps:add(self, k, d)
   self.arrayLoc = self.arrayLoc + 1
   self.key[self.arrayLoc] = k
   self.dataType[self.arrayLoc] = d
 end
+]]
 
+--[[ Purpose: This function is for adding a player on a specific tile
 
+]]
 
-function TileMaps:onTile(self, tileLoc, player)
+function TileMaps:addPlayerOnTile(self, tileLoc, player)
   loc = 0
   for i = 1, table.getn(self.key), 1 do
     if (tileLoc == self.key[i]) then
@@ -33,7 +61,7 @@ function TileMaps:onTile(self, tileLoc, player)
   self.dataType[loc][nEmptyTile + 1] = player
 end
 
-function TileMaps:offTile(self, tileLoc, player)
+function TileMaps:removePlayerOnTile(self, tileLoc, player)
   loc = 0
   locPlayer = 0
   for i = 1, table.getn(self.key), 1 do
